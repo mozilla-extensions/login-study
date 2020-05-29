0bd6ef584c335f3849d244d390eb39c8a65d0db9
Erica Wright <ericaewright@gmail.com>
2020-05-28 20:13:49 -0400

    Begin login study

--- src/background.js
+++ src/background.js
@@ -1,5 +1,38 @@
+
+
 browser.browserAction.onClicked.addListener(async () => {
   await browser.tabs.create({
     url: "debug-page/index.html",
   });
+
+  const sendTelemetry = async () => {
+
+    const password_manager_enabled = await browser.prefs.getBoolPref("signon.rememberSignons");
+    const browser_privatebrowsing_autostart = await browser.prefs.getBoolPref("browser.privatebrowsing.autostart");
+    const network_cookie_lifetime_policy = await browser.prefs.getIntPref("network.cookie.lifetimePolicy");
+    const privacy_sanitize_sanitize_on_shutdown = await browser.prefs.getBoolPref("privacy.sanitize.sanitizeOnShutdown");
+    const privacy_clear_on_shutdown_cookies = await browser.prefs.getBoolPref("privacy.clearOnShutdown.cookies");
+    const network_cookie_cookie_behavior = await browser.prefs.getIntPref("network.cookie.cookieBehavior");
+    const browser_startup_page = await browser.prefs.getIntPref("browser.startup.page");
+
+    const payload = {
+      loginstudy:
+      {
+        password_manager_enabled,
+        browser_privatebrowsing_autostart,
+        network_cookie_lifetime_policy,
+        privacy_sanitize_sanitize_on_shutdown,
+        privacy_clear_on_shutdown_cookies,
+        network_cookie_cookie_behavior,
+        browser_startup_page,
+
+      },
+    };
+
+
+    // addClientId defaults to false, being explicit.
+    TelemetryController.submitExternalPing("normandy-login-study", payload, {addClientId: false});
+  };
+
+  await sendTelemetry();
 });
---
+++ src/privileged/prefs/api.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/* global ExtensionAPI */
+
+const {Services} = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+/* https://firefox-source-docs.mozilla.org/toolkit/components/extensions/webextensions/functions.html */
+this.prefs = class extends ExtensionAPI {
+  getAPI(context) {
+    return {
+      prefs: {
+        getStringPref: Services.prefs.getStringPref,
+        getIntPref: Services.prefs.getIntPref,
+        getBoolPref: Services.prefs.getBoolPref,
+      },
+    };
+  }
+};
